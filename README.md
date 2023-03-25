32a8cbdfc900eefd0b8eb62420930cfad78a23a0
idealaide3 <idealaide3@Demos-MacBook-Pro.local>
2023-03-25 10:34:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# idealaide3
---
+++ README.php
@@ -0,0 +1,24 @@
+# thedev.id
+
+An identity for developers on the web.
+
+## What is thedev.id
+
+**thedev.id** is a free identity for developers on the web. This service offers a handy subdomain like `foo.thedev.id` or `bar.thedev.id` for use on your static sites via [CNAME record](https://en.wikipedia.org/wiki/CNAME_record). Fork, add subdomain and send a Pull Request. We will make it work.
+
+## How to get one
+
+- Star this repo.
+- Fork this repo.
+- Set a custom domain to your [GitHub Pages](https://pages.github.com).
+- Add your subdomain under `subdomains.json` file.
+- Run `npm install` and `npm run sort` to sort the keys alphabetically.
+- Send Pull Request.
+
+### Similar Services
+- [Open Domains](https://github.com/open-domains/register)
+- [cluster.ws & wip.la](https://github.com/Olivr/free-domain)
+- [is-a.dev](https://github.com/is-a-dev/register)
+- [is-a-good.dev](https://github.com/is-a-good-dev/register)
+- [is-really.cool](https://github.com/is-really-cool/register)
+- [js.org](https://github.com/js-org/js.org) (*JavaScript projects only*)
